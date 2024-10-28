716766a70a8e5d6e4f66b054ad8419cb79b22ef7
AndrewChernyakov <158245262+AndrewChernyakov@users.noreply.github.com>
2024-10-27 23:24:25 -0400

    Add files via upload

---
+++ treeLabTesting.java
@@ -0,0 +1,17 @@
+
+public class treeLabTesting {
+
+	public static void main(String[] args) {
+		
+		
+		int intNotation = ( ((48-(7%2) / 24) * ((18 - (5*2)) + 12)) );
+		
+
+		System.out.println("int notation: " + intNotation);
+		
+		float floatNotation = ( ((48-(7&2) / 24) * ((18 - (5*2)) + 12)) );
+		
+		System.out.println("float notation: " + floatNotation);
+	}
+
+}
